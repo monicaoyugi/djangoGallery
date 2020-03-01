3d96f21364da5f257e8142b82fa1590194c0e8de
monicaoyugi <monicaoyugi@gmail.com>
2020-03-01 16:17:06 +0300

    formed url patterns for template functions

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import  static
 
 urlpatterns=[
-    path('',views.Gallery,name = 'Gallery'),
+    path('',views.index, name = 'indexPage'),
+    path('',views.search_results, name='search_results'),
+    path('', views.filter_by_location, name='location'),
+    path('',views.filter_by_category, name='category'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL_ROOT)
 
