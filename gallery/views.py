271f61a3903e09a48fa5fe1ff335e1aa3ba3e2b0
monicaoyugi <monicaoyugi@gmail.com>
2020-03-01 16:19:40 +0300

    takes a web request and return a web response.

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Image
+
+def index(request):
+    images = Image.objects.all()
+    return render(request,'gallery/index.html',{"images":images})
+
+def search_results(request):
+    if 'image' in request.Get and request.Get["image"]:
+        search_term = request.GET.get("image")
+        searched_Images = Image.search_image(search_term)
+        message = f"{search_term}"
+
+        return render(request,'gallery/search.html',{"message":massage,"Images":searched_Images})
+
+    else:
+        message = " You didn't search for anything."
+        return render(request,'gallery/search.html',{"message":message})
+
+def search_location(request,location):
+    images_by_location = Image.filter_by_location(location)
+
+    return render(request,'gallery/location.html',{"images":images_by_location})
+
+
