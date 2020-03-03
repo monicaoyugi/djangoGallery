ef3ae91e41bfa9fdd4f757593a0ba031a5a69340
monicaoyugi <monicaoyugi@gmail.com>
2020-03-03 11:00:54 +0300

    inout comment

--- gallery/models.py
+++ gallery/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Location(models.Model):
-    place = models.CharField(max_length=255)
+    place = models.CharField(max_length=255) #input location
 
     def save_location(self):
         self.save()
